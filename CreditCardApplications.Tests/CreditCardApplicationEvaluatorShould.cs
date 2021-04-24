fbed402dcd2190aaa478d15ba84fb28b4d873d82
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 08:47:33 +0200

    Mock method with out parameter

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -86,5 +86,27 @@ namespace CreditCardApplications.Tests
 
             Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
         }
+
+        [Fact]
+        public void DeclineLowIncomeApplicationsOutDemo()
+        {
+            Mock<IFrequentFlyerNumberValidator> mockValidator =
+                new Mock<IFrequentFlyerNumberValidator>(MockBehavior.Strict);
+
+            bool isValid = true;
+            mockValidator.Setup(x => x.IsValid(It.IsAny<string>(), out isValid));
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+
+            var application = new CreditCardApplication()
+            {
+                GrossAnnualIncome = 19_999,
+                Age = 42
+            };
+
+            CreditCardApplicationDecision decision = sut.EvaluateUsingOut(application);
+
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decision);
+        }
     }
 }
