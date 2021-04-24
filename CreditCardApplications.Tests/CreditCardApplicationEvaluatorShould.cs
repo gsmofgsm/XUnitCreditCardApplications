03c88fbf598d938dfd109c64de54d14d03c394b4
Qing Ma <q.ma@ncontrol.cloud>
2021-04-24 11:17:08 +0200

    SetupSequence with differenct returns

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -284,5 +284,30 @@ namespace CreditCardApplications.Tests
 
             Assert.Equal(1, sut.ValidatorLookupCount);
         }
+
+        [Fact]
+        public void ReferInvalidFrequentFlyerApplication_ReturnValuesSequence()
+        {
+            var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
+
+            mockValidator.Setup(x => x.ServiceInformation.License.LicenseKey).Returns("OK");
+
+            mockValidator.SetupSequence(x => x.IsValid(It.IsAny<string>()))
+                .Returns(false)
+                .Returns(true);
+
+            var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
+
+            var application = new CreditCardApplication
+            {
+                Age = 25
+            };
+
+            CreditCardApplicationDecision firstDecision = sut.Evaluate(application);
+            Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, firstDecision);
+
+            CreditCardApplicationDecision secondDecision = sut.Evaluate(application);
+            Assert.Equal(CreditCardApplicationDecision.AutoDeclined, secondDecision);
+        }
     }
 }
